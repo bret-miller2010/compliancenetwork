3d5c9cff8f8d684e6f7016d87afffaf96ffd6a1d
Bret Miller <bret.miller2015@gmail.com>
2025-05-08 09:34:34 -0500

    created folder structure, main page, and metrics page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/** @format */
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.tsx";
+import MetricsPage from "./pages/MetricsPage.tsx";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key");
+}
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <ClerkProvider
+                publishableKey={PUBLISHABLE_KEY}
+                afterSignOutUrl="/">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<App />}
+                    />
+                    <Route
+                        path="/metrics"
+                        element={<MetricsPage />}
+                    />
+                </Routes>
+            </ClerkProvider>
+        </BrowserRouter>
+    </StrictMode>
+);
---
+++ src/pages/MetricsPage.tsx
@@ -0,0 +1,10 @@
+const MetricsPage = () => {
+  return (
+    <div>
+      <h1>Metrics</h1>
+      <p>Metrics page content goes here.</p>
+    </div>
+  );
+}
+
+export default MetricsPage;
